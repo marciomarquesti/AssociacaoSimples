b6674887944d8d4d9f5a63aecaa61ea5c850eb9f
marciomarquesti <marciomarquesdiasti@gmail.com>
2018-04-11 21:03:26 -0300

    Primeira versão do código

--- src/br/com/marcio/modelo/MainAssociacaoSimples.java
+++ src/br/com/marcio/modelo/MainAssociacaoSimples.java
@@ -1,5 +1,16 @@
 package br.com.marcio.modelo;
 
+/*
+ * A associação é uma das formas mais simples de relacionamento entre classes 
+ * e ocorre quando existe uma classe que possui um atributo de outra classe
+ * objetos ou array de objetos ).
+ *
+ * Este tipo de relacionamento também é conhecido por relacionamento ?tem um(a)?.
+ *	
+ * Por exemplo, quando uma associação entre a classe ?Pessoa? e a classe ?Endereco?, 
+ * diz que uma pessoa ?tem um? endereço.
+ */
+
 public class MainAssociacaoSimples {
 
 	public static void main(String[] args) {
